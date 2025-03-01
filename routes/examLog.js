7ecfa4676d367edcec8b1108144769fc218735f4
Supakorn Chat-anothai <102203151+64130500111@users.noreply.github.com>
2025-03-01 16:11:13 +0700

    🎲 update:

--- routes/examLog.js
+++ routes/examLog.js
@@ -17,11 +17,8 @@ router.get("/examID", auth, getAllExamLogID);
 //แก้ สรุปใช้อยุ่ไหม
 // router.post("/question", getQuestionDetailByExamIDAndQuestionID);
 router.put("/select/", auth, updateSelectOption);
-//แก้ ใช้ :exam_id
 router.put("/:exam_id/submit", auth, checkAnswer);
-//แก้ ใช้ :exam_id + METHOD GET
 router.get("/:exam_id/count", auth, getCountQuestionByExamID);
-//แก้ ชื่อ path + METHOD GET + :exam_id
 router.get("/:exam_id/detail", auth, getExamTestedDetail);
 
 export default router;
